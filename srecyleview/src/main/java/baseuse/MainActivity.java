622a42560a962c9f955fc3648ff3731ab6099750
Administrator <Jasonsens17@gamil.com>
2017-08-31 11:58:52 +0800

    recycleview addheader footer 有一个bug ：删除最后一个时，待解决

--- srecyleview/src/main/java/baseuse/MainActivity.java
+++ srecyleview/src/main/java/baseuse/MainActivity.java
@@ -2,13 +2,16 @@ package baseuse;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.helper.ItemTouchHelper;
+import android.view.LayoutInflater;
+import android.view.View;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import baseuse.itemdecoration.GridItemDecoration;
+import baseuse.commadapter.HeadFootAdapter;
+import baseuse.itemdecoration.LineanItemDecorationV2;
 import itemtouchhelper.SItemTouchCallback;
 import sen.com.srecyleview.DataItems;
 import sen.com.srecyleview.R;
@@ -24,10 +27,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_item_helper);
 
         recyclerView = (SRecyclerView) findViewById(R.id.recycleView);
-//        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
-//        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
-        GridLayoutManager gridLayoutManager = new GridLayoutManager(this,3);
-        recyclerView.setLayoutManager(gridLayoutManager);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+
+       // GridLayoutManager gridLayoutManager = new GridLayoutManager(this,3);
+        recyclerView.setLayoutManager(linearLayoutManager);
         List<DataItems> dataItemses = new ArrayList<>();
         for (int i=0 ;i<20;i++){
             dataItemses.add(new DataItems("SItem"+i, colorArray[i % colorArray.length]));
@@ -35,9 +39,15 @@ public class MainActivity extends AppCompatActivity {
 
         baseuse.ItemAdapter adapter = new baseuse.ItemAdapter(this,dataItemses,R.layout.rv_item);
         recyclerView.setHasFixedSize(true);
-//        recyclerView.addItemDecoration(new LineanItemDecorationV2(this,R.drawable.item_decoration));
-        recyclerView.addItemDecoration(new GridItemDecoration(this,R.drawable.item_decoration));
-        recyclerView.setAdapter(adapter);
+        recyclerView.addItemDecoration(new LineanItemDecorationV2(this,R.drawable.item_decoration));
+       // recyclerView.addItemDecoration(new GridItemDecoration(this,R.drawable.item_decoration));
+        HeadFootAdapter headerViewListAdapter = new HeadFootAdapter(adapter);
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View view =inflater.inflate(R.layout.item_header_footer,recyclerView,false);
+
+        headerViewListAdapter.addHeader(view);
+        headerViewListAdapter.addFooter(view);
+        recyclerView.setAdapter(headerViewListAdapter);
 
        new ItemTouchHelper(new SItemTouchCallback(adapter)).attachToRecyclerView(recyclerView);
 
